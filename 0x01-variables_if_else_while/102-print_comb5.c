ef2f220e670cd37791238d499abe9c18bfbb5247
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-06 15:40:00 -0700

    okkayyy

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    int i, j;
+int main(void) {
+    int num1, num2;
 
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
-            putchar((i / 10) + '0');
-            putchar((i % 10) + '0');
+    for (num1 = 0; num1 <= 99; num1++) {
+        for (num2 = num1; num2 <= 99; num2++) {
+            // Extract tens and ones digits for num1 and num2
+            int tens1 = num1 / 10;
+            int ones1 = num1 % 10;
+            int tens2 = num2 / 10;
+            int ones2 = num2 % 10;
+
+            // Print tens digit of num1
+            putchar('0' + tens1);
+            // Print ones digit of num1
+            putchar('0' + ones1);
+            // Space separator
             putchar(' ');
-            putchar((j / 10) + '0');
-            putchar((j % 10) + '0');
-            putchar(',');
-            putchar(' ');
+
+            // Print tens digit of num2
+            putchar('0' + tens2);
+            // Print ones digit of num2
+            putchar('0' + ones2);
+
+            // Check if it's not the last combination
+            if (num1 != 99 || num2 != 99) {
+                // Comma and space separator
+                putchar(',');
+                putchar(' ');
+            }
         }
     }
+
+    putchar('\n');
 
     return 0;
 }
