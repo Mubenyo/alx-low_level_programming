e7fadc787732ce912ab7dd41d930c9a1096cd54e
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-06 15:33:31 -0700

    ok

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 
-/**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
- */
+#define MIN_DIGIT '0'
+#define MAX_DIGIT '9'
 
-int main(void)
-{
-	int tens;
-	int ones;
-	int t;
-	int o;
+void printPair(char tens, char ones, char t, char o) {
+    putchar(tens);
+    putchar(ones);
+    putchar(' ');
+    putchar(t);
+    putchar(o);
+}
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+int main(void) {
+    char tens, ones, t, o;
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
+    for (tens = MIN_DIGIT; tens <= MAX_DIGIT; tens++) {
+        for (ones = MIN_DIGIT; ones <= MAX_DIGIT; ones++) {
+            for (t = tens; t <= MAX_DIGIT; t++) {
+                for (o = (tens == t) ? (ones + 1) : MIN_DIGIT; o <= MAX_DIGIT; o++) {
+                    printPair(tens, ones, t, o);
 
-	return (0);
+                    if (!((tens == MAX_DIGIT && ones == MAX_DIGIT) && (t == MAX_DIGIT && o == MAX_DIGIT))) {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
 }
