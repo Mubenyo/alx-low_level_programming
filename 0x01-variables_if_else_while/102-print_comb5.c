79f92847d922a335e7741da998861dc5f682680a
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-06 15:35:37 -0700

    okk

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
 
-#define MIN_DIGIT '0'
-#define MAX_DIGIT '9'
+int main() {
+    int i, j;
 
-void printPair(char tens, char ones, char t, char o) {
-    putchar(tens);
-    putchar(ones);
-    putchar(' ');
-    putchar(t);
-    putchar(o);
-}
-
-int main(void) {
-    char tens, ones, t, o;
-
-    for (tens = MIN_DIGIT; tens <= MAX_DIGIT; tens++) {
-        for (ones = MIN_DIGIT; ones <= MAX_DIGIT; ones++) {
-            for (t = tens; t <= MAX_DIGIT; t++) {
-                for (o = (tens == t) ? (ones + 1) : MIN_DIGIT; o <= MAX_DIGIT; o++) {
-                    printPair(tens, ones, t, o);
-
-                    if (!((tens == MAX_DIGIT && ones == MAX_DIGIT) && (t == MAX_DIGIT && o == MAX_DIGIT))) {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
+    for (i = 0; i < 100; i++) {
+        for (j = i; j < 100; j++) {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+            putchar(',');
+            putchar(' ');
         }
     }
 
-    putchar('\n');
-
     return 0;
 }
+
