64e4a0b9c68ab425662629b4008f6940704c139a
mustapha bouhou <57944084+Mubenyo@users.noreply.github.com>
2023-10-06 23:51:39 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    int num1, num2;
-
-    for (num1 = 0; num1 <= 99; num1++) {
-        for (num2 = num1 + 1; num2 <= 99; num2++) {
-       
+    for (int num1 = 0; num1 <= 99; num1++) {
+        for (int num2 = num1 + 1; num2 <= 99; num2++) {
+         
             int tens1 = num1 / 10;
             int ones1 = num1 % 10;
             int tens2 = num2 / 10;
             int ones2 = num2 % 10;
 
-          
+  
             putchar('0' + tens1);
             putchar('0' + ones1);
             putchar(' ');
             putchar('0' + tens2);
             putchar('0' + ones2);
 
-         
-            if (num1 != 99 || num2 != 98) {
+        
+            if (!(num1 == 99 && num2 == 98)) {
            
                 putchar(',');
                 putchar(' ');
@@ -31,3 +29,4 @@ int main(void) {
 
     return 0;
 }
+
