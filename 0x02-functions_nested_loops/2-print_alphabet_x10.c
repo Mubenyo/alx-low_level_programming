556c0962cf112e61faa08f04bf760a7495832794
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-10 14:44:05 -0700

    alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@ void print_alphabet_x10(void)
 {
 	int i;
 	for (i = 0; i < 10; i++)
-       	{
-        char alphabet = 'a';
-
-        while (alphabet <= 'z') {
-            putchar(alphabet);
-            alphabet++;
-        }
-
-        putchar('\n');
-    }
+	{
+		char alphabet = 'a';
+		
+		while (alphabet <= 'z')
+		{
+			putchar(alphabet);
+			alphabet++;
+		}
+		putchar('\n');
+	}
 }        
