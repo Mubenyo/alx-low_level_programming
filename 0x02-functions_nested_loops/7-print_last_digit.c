a30c72797f3198d8385b3cdb16a716afad7b298d
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-10 16:26:45 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <holberton.h>
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int number)
+{
+	if (number < 0)
+		number *= -1;
+
+       	putchar('0' + (n % 10));
+
+	return (number % 10);
+}
