0a21243901c3b6fd82f3da86b4de1e57540d4aa8
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-10 16:11:11 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 
 
-/*int print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
@@ -24,12 +24,4 @@
 		putchar('-');
 		return (-1);
 	}
-}*/
-int print_sign(int n) {
-    if (n >= 0) {
-        putchar('+');
-    } else {
-        putchar('-');
-    }
-    return 0;
 }
