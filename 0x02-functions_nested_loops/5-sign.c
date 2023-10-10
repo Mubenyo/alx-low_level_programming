da3462d3791d7622709866755739fa42e83649f1
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-10 16:03:42 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,29 +6,22 @@
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
 
-int print_sign(int num)
+
+int print_sign(int n)
 {
-	if (num > 0)
+	if (n > 0)
 	{
-		putchar('+');
-		putchar(',');
-		putchar(' ');
-		putchar('1');
-		return(1);
+		_putchar('+');
+		return (1);
 	}
-	else if (num == 0)
+	else if (n == 0)
 	{
-		putchar('0');
-		putchar(',');
-                putchar(' ');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar(',');
-                putchar(' ');
-		putchar(',');
-                putchar(' ');
+		_putchar('-');
 		return (-1);
 	}
 }
