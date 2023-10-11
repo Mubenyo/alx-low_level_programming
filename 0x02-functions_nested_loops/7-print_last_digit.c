6f7930477b76816e32361290b5dd36869cd5ae1a
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-11 13:40:37 -0700

    last digit update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,17 @@
  * Return: last digit of number
  */
 
-int print_last_digit(int number)
+int print_last_digit(int n)
 {
-	if (number < 0)
-		number *= -1;
+    int last_digit;
 
-       	putchar('0' + (number % 10));
+    if (n < 0)
+        n = -n;
 
-	return (number % 10);
+    last_digit = n % 10;
+
+    _putchar(last_digit + '0');
+
+    return (last_digit);
 }
+
