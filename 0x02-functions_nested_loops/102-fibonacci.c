89ef48a30561d15de8a8b3eaa5473a0cac37e641
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-11 14:43:34 -0700

    fibonaci update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,31 +7,34 @@
 
 #include <stdio.h>
 
-int main(void) {
-    int counter;
-    int countto = 50;
-    long a = 1;
-    long b = 2;
-    long temp;
+int main(void)
+{
+	int counter;
+	int countto = 50;
+	long a = 1;
+	long b = 2;
+	long temp;
 
-    printf("%li", a);
+	printf("%li", a);
 
-    for (counter = 2; counter <= countto; counter++) {
+	for (counter = 2; counter <= countto; counter++) {
         printf(", %li", b);
 
-        if (counter % 2 == 0) {
+        if (counter % 2 == 0)
+	{
             temp = a + b;
             a = b;
             b = temp;
-        } else {
+        } else
+	{
             temp = a + b;
             a = b;
             b = temp;
         }
     }
 
-    printf("\n");
-
-    return 0;
+ 	printf("\n");
+	
+	return 0;
 }
 
