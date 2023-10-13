1cfac7ce9fd8b8af71ff8978a3f3593ad6e9615b
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-13 08:21:27 -0700

    is upper update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,16 @@
  * Return: 1 if true, 0 if false
  */
 
+#include <stdio.h>
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-
-	return (0);
+    if (c >= 'A' && c <= 'Z')
+    {
+    return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
