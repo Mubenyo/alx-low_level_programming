de7ef5021a5524d04ba04110eae504604138d0c3
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-13 11:19:32 -0700

    print_number updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,21 @@
  */
 
 
+int _putchar(char c) 
+{
+    return write(1, &c, 1);
+}
+
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_number(n / 10);
-	putchar(n % 10 + '0');
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+    if (n / 10 != 0)
+    {
+        print_number(n / 10);
+    }
+    _putchar(n % 10 + '0');
 }
