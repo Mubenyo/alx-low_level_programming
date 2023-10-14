6e52a3855cc3112a6207d4a4cda16a9b58317ad2
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-14 01:10:15 -0700

    print_number update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,7 +5,7 @@
  * _putchar - Writes a character to standard output
  * @c: The character to print
  * Return: The number of characters printed
- */
+
 
 
 int _putchar(char c) 
@@ -25,4 +25,36 @@ void print_number(int n)
         print_number(n / 10);
     }
     _putchar(n % 10 + '0');
+}
+*/
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
+
+
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+
+	k /= 10;
+
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+
 }
