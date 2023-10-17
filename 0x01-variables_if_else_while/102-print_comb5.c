d945092ee386359be82cb330d15fb49f6d0fa481
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-17 05:22:35 -0700

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include "main.h"
-
-/**
- * main - Entry point for the program.
- *
- * Description: This program prints all possible combinations of two two-digit
- * numbers in the format "XX YY," separated by commas and spaces. It iterates
- * through all two-digit numbers, ensuring that the second number is greater
- * than or equal to the first number.
- *
- * Return: Always returns 0 to indicate successful execution.
+/*
+ * File: 102-print_comb5.c
+ * Auth: Brennan D Baraban
  */
 
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			int tens1 = num1 / 10;
-			int ones1 = num1 % 10;
-			int tens2 = num2 / 10;
-			int ones2 = num2 % 10;
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			_putchar('0' + tens1);
-			_putchar('0' + ones1);
-			_putchar(' ');
-			_putchar('0' + tens2);
-			_putchar('0' + ones2);
-			if (num1 != 98 || num2 != 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
