c4fe4d3c0ded857ad96b64b097a9d1dca1f63a34
Mustapha Bouhou <mus.bouhou@gmail.com>
2023-10-19 14:53:43 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,19 +1,36 @@
-#include <stdio.h>
-#include <ctype.h>
+#include "main.h"
 
-char *rot13(char *str) {
-    if (str == NULL) {
-        return NULL;
-    }
+/**
+ *rot13 - encodes strings using rot13.
+ *@s: pointer to string.
+ *
+ *Return: pointer to encoded string.
+ */
+char *rot13(char *s)
+{
+	int stringCount, rotate;
+	char x[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z'};
 
-    for (char *ptr = str; *ptr; ++ptr) {
-        char c = *ptr;
+	char y[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M'};
 
-        if (isalpha(c)) {
-            char base = (islower(c)) ? 'a' : 'A';
-            *ptr = (c - base + 13) % 26 + base;
-        }
-    }
-
-    return str;
+	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
+	{
+		for (rotate = 0; rotate < 53; rotate++)
+		{
+			if (x[rotate] == s[stringCount])
+			{
+				s[stringCount] = y[rotate];
+				break;
+			}
+		}
+	}
+	return (s);
 }
