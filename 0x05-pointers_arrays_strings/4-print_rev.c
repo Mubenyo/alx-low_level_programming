e3dd3f98b4924caf936a94d1544b0c2a2990682e
Mubenyo <mus.bouhou@gmail.com>
2023-11-10 08:44:21 -0800

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,25 +8,22 @@
 void print_rev(char *s)
 {
 
-	int len = 0;
+	int length = 0;
 	int i;
 
 	/* iterate to find length of string and point to last character */
-	while (*s != '\0')
+	while (*s)
 	{
-		len++;
-		++s;
+		length++;
+		s++;
 	}
-
-	/* go back to character before null character */
-	s--;
-
+	
 	/* print string reversed */
-	for (i = len; i > 0; i--)
+	for (i = length + 1; i > 0; i--)
 	{
-		_putchar(*s);
+		putchar(*s);
 		s--;
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
